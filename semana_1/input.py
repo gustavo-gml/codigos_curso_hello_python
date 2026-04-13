12a8cc2905eaf4681bf513722b9a553fb276d9fa
josegabtech <jose.gabriel.tech@gmail.com>
2026-04-13 16:49:02 -0300

    13/04

---
+++ semana_1/input.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+print(f"Olá {nome} que tem {idade} anos!")
+
+print(type(idade))
---
+++ semana_1/type.py
@@ -0,0 +1,12 @@
+nome = "José"
+idade = 19
+troco = 1.50
+ativa = True
+
+print(type(nome))
+print(type(idade))
+print(type(troco))
+print(type(ativa))
+
+print("15" + "15")
+print(15 + 15)
